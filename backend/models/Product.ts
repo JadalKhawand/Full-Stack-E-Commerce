816ac3a9af3a312e0f2a2e23e0c120d35bd64010
Jad alKhawand <jkh42005@gmail.com>
2024-02-10 20:22:24 +0200

    Product model is done

--- backend/models/Product.ts
+++ backend/models/Product.ts
@@ -1,5 +1,51 @@
 import mongoose from "mongoose";
 
-const ProductSchema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
-const productSchema = new ProductSchema({})
+const ProductSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  store: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Store",
+    required: true,
+  },
+  category:{
+    type: String,
+    required: true,
+  },
+  pictures:{
+    type: [String],
+    required: true,
+  },
+  sumOfRatings: {
+    type: Number,
+    default: 0
+},
+numberOfReviews: {
+    type: Number,
+    default: 0
+},
+updatedAt: {
+    type: Date,
+    default: Date.now
+},
+createdAt: {
+    type: Date,
+    default: Date.now
+},
+});
+
+const Product = mongoose.model("Product", ProductSchema);
+export default Product;
+
