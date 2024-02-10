6944252b36096ef235d6e56948f514c13fcf8696
Jad alKhawand <jkh42005@gmail.com>
2024-02-10 22:45:37 +0200

    Auth middleware is done

--- backend/index.ts
+++ backend/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import UserController from "./middleware/UserController";
+import UserController from "./controllers/UserController";
 
 dotenv.config();
 
---
+++ backend/middleware/auth.ts
@@ -0,0 +1,33 @@
+import express, { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+const secret = process.env.SECRET_TOKEN;
+
+interface AuthenticatedRequest extends Request {
+  decoded: any; 
+}
+
+if (!secret)
+  throw new Error("Please add the TOKEN_SECRET variable in .env file");
+
+
+export const auth = (req: AuthenticatedRequest, res: Response, next: Function) => {
+  try {
+    const token = req.get("Authorization")?.split(" ")[1];
+
+    if (!token) {
+      return res.status(403).send({ error: "Token missing!" });
+    }
+
+    const decoded = jwt.verify(token, secret);
+
+    if (decoded) {
+      req.decoded = decoded;
+      next();
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error: "Failed to verify JWT" });
+  }
+};
